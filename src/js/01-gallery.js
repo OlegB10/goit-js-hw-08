0650f084489897728c7dcf15643b6bb4338f14e2
OlegB10 <oleg_b10@ukr.net>
2023-05-28 07:52:19 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEL = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const imagesList = ({preview, original, description}) => {
+    return `  <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+            </a>`;
+  };
+
+const addImg = galleryItems.map(imagesList).join('');
+galleryEL.insertAdjacentHTML("afterbegin", addImg);
+
+galleryEL.addEventListener('click', onImgClick );
+
+function onImgClick (event){
+const imageSelected = event.target.getAttribute("data-source");
+  
+event.preventDefault();
+
+if (!imageSelected){return;}
+};
+
+new SimpleLightbox(".gallery a", {captionDelay: 250 });
